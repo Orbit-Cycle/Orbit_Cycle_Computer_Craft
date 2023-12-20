af8c26f547448f94ff66175ec6e5569f657923bf
MikeyPants <michael.a.hazan@gmail.com>
2023-12-20 20:06:46 +0200

    yup

--- Display/Controller.lua
+++ Display/Controller.lua
@@ -4,19 +4,10 @@
 local protocol = "bussyPhui9076@"
 
 local filename = "DisplayTables.lua" -- filename to access for tables
-local repo = "" -- github link
+local repo = "https://raw.githubusercontent.com/Michael-Hazan/Orbit_Cycle_CC/main/Tables/RulesTable.lua?token=GHSAT0AAAAAACLQTGHDPFW7UQ3NWQDARC7YZMBZFBA" -- github link
 
-
-peripheral.find("modem", rednet.open)
-
-rednet.host(protocol, "Controller")
-
+-- monitors IDS
 local monitors = {}
-local command = "start"
-
-if not fs.exists(filename) then
-    shell.run("wget ".. filename .." ".. repo)
-end
 
 
 local function Contains(table, val)
@@ -43,35 +34,46 @@ local function Dump(o)
    end
 end
 
+local function loadTables()
+    local website = http.get(repo)
+    if website then
+        shell.run("wget " .. repo .. " " .. filename)
+        print("Downloaded tables!")
+    else
+        print("Failed to get tables! Website not found.")
+    end
+end
 
+
+-- runtime Function, activates all of the displays 
 local function run()
     rednet.broadcast("startupSpawnDisplays -" .. protocol)
     sleep(0.5)
-	print("Searching for monitors")
+    print("Searching for monitors")
     local id, message = rednet.receive(protocol)
-	local i = 1
-	while message ~= nil do
+    local i = 1
+    while message ~= nil do
         if message == "DisplayMonitor" then
-			if not Contains(monitors, id) then
-            	monitors[i] = id
-            	i = i + 1
-			end
-			rednet.broadcast("Recieved ".. id, protocol)
-		end
-		id, message = rednet.receive(protocol, 1)
-	end
+            if not Contains(monitors, id) then
+                monitors[i] = id
+                i = i + 1
+            end
+            rednet.broadcast("Recieved " .. id, protocol)
+        end
+        id, message = rednet.receive(protocol, 1)
+    end
 
     print("Monitors IDS: (Connected)\n" .. Dump(monitors))
-    
+
     while true do
         local table1 = {
-            Color= colors.blue,
+            Color = colors.blue,
         }
         local table2 = {
-            Color= colors.red,
+            Color = colors.red,
         }
         local table3 = {
-            Color= colors.green,
+            Color = colors.green,
         }
         rednet.broadcast(table1, protocol)
         sleep(1)
@@ -80,25 +82,76 @@ local function run()
         rednet.broadcast(table3, protocol)
         sleep(1)
     end
-
 end
+
+--
+-- RUN TIME CODE --
+--
+
+-- opens rednet
+peripheral.find("modem", rednet.open)
+rednet.host(protocol, "Controller")
+
+-- current command
+local command = "start"
+
+-- functions to commands
+local functions =  {
+    run = run,
+    loadtables = loadTables,
+    monitors = function()
+        print(Dump(monitors))
+    end,
+    test = function()
+        rednet.broadcast("Testing...", protocol)   
+    end,
+    help = function()
+        print(
+            "stop - Stops the program \n"..
+            "run - starts up all of the display monitors (make sure the program -DisplayMonitor- runs on them as well)\n"..
+            "test - sends a test message\n"..
+            "monitors - gives a list of monitors IDS that are connected\n"..
+            "loadtables - redownloads the tables"
+        )
+    end,
+    stop = function()
+        print("bye bye!")
+    end
+}
+
+
+
+if not fs.exists(filename) then
+    local website = http.get(repo)
+    if website then
+        shell.run("wget " .. repo .. " " .. filename)
+        print("Downloaded tables!")
+    else
+        print("Failed to get tables! Website not found.")
+    end
+else
+    print("Tables already loaded")
+end
+
 
 print("Loaded! type help for list of commands")
 while command ~= "stop" do
-
     command = io.read()
-  if command == "test" then
+    if command == "test" then
         rednet.broadcast(rednet.lookup(protocol), protocol)
-	elseif command == "run" then
+    elseif command == "run" then
         run()
-	elseif command == "monitors" then
-		    monitorsCheck()
-	elseif command == "help" then
-        print("stop - Stops the program \n run - starts up all of the display monitors (make sure the program -DisplayMonitor- runs on them as well)")
-	elseif command == "stop" then
-		print("bye bye")
-  else
-		print("invalid command.")
-	end
+    elseif command == "monitors" then
+        print(Dump(monitors))
+    elseif command == "help" then
+        print(
+        "stop - Stops the program \n run - starts up all of the display monitors (make sure the program -DisplayMonitor- runs on them as well)")
+    elseif command == "stop" then
+        print("bye bye")
+    else
+        print("invalid command.")
+    end
 end
+
+
 rednet.unhost("Controller")
--- Tables/RulesTable.lua
+++ Tables/RulesTable.lua
@@ -1,3 +1,11 @@
-local table = {
-	"Title"
+---@diagnostic disable: undefined-global, undefined-field
+local TitleScreen = {
+    BackgroundColor = colors.red,
+    Title = {
+        Text = "Hello",
+        Centered = false,
+        Color = colors.blue,
+		
+	}
 }
+return {TitleScreen}
